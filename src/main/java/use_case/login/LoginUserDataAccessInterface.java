2a5cb8932b38b4bd56c7a84f3d1a2b9e7aa10999
aotian69 <ao.tian@mail.utoronto.ca>
2024-10-21 22:49:53 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set the user with the given username.
+     * @param name the username for the current user.
+     */
+    void setCurrentUser(String name);
 }
